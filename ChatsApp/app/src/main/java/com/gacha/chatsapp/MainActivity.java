0fd54918df789644bdabacf4cb57813a934f67d5
baobaote00 <baobaote00@gmail.com>
2021-04-11 15:15:26 +0700

    :heart:

--- ChatsApp/app/src/main/java/com/gacha/chatsapp/MainActivity.java
+++ ChatsApp/app/src/main/java/com/gacha/chatsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gacha.chatsapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message);
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.hide();
     }
 }
