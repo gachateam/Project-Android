891e7c568798d2b64e32655f4a44e32c46ca562d
baobaote00 <baobaote00@gmail.com>
2021-05-05 02:18:11 +0700

    :heart:

--- QuizApp/app/src/main/java/com/gacha/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/gacha/quizapp/MainActivity.java
@@ -3,14 +3,18 @@ package com.gacha.quizapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Dialog myDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
         btn.setOnClickListener(v -> myRef.setValue("lo cc"));
 
+        myDialog = new Dialog(this);
+    }
+
+    public void showPopup(View v){
+        TextView txtClose;
+        Button btnOk ;
+        //   myDialog.setContentView(R.id.);
+        txtClose = (TextView) myDialog.findViewById(R.id.close);
+        btnOk = (Button) myDialog.findViewById(R.id.ok);
+        txtClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        myDialog.show();
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        myDialog.show();
     }
 }
