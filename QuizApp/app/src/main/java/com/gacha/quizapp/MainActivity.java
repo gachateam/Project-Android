a22b25ce3cda5601f4d1550612f4b9f1e11e42cf
anhtrinh841 <80993203+anhtrinh841@users.noreply.github.com>
2021-05-06 10:48:54 +0700

    push file

--- QuizApp/app/src/main/java/com/gacha/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/gacha/quizapp/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.viewpager.widget.ViewPager;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
@@ -36,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
     public void showPopup(View v){
         TextView txtClose;
         Button btnOk ;
+        ImageView image;
         //   myDialog.setContentView(R.id.);
         txtClose = (TextView) myDialog.findViewById(R.id.close);
         btnOk = (Button) myDialog.findViewById(R.id.ok);
+        image = (ImageView) myDialog.findViewById(R.id.hinh);
         txtClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,9 +58,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 myDialog.dismiss();
+
             }
         });
         myDialog.show();
 
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        image.setAnimation(animation);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+                image.setAnimation(animation1);
+            }
+        });
+
     }
+
 }
