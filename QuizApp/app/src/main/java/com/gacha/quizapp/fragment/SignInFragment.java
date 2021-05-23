3ce7dbba127a4218b11e2e297f64daba172814b6
Yamikur0 <68624487+Yamikur0@users.noreply.github.com>
2021-05-23 16:52:07 +0700

    done

--- QuizApp/app/src/main/java/com/gacha/quizapp/fragment/SignInFragment.java
+++ QuizApp/app/src/main/java/com/gacha/quizapp/fragment/SignInFragment.java
@@ -4,14 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.gacha.quizapp.R;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 public class SignInFragment extends Fragment {
+    private View signInFragment;
+    private EditText email, password;
+    private Button btnSignIn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.signin_fragment, container, false);
+        signInFragment = inflater.inflate(R.layout.signin_fragment, container, false);
+
+        email = signInFragment.findViewById(R.id.editTextTextEmailAddress);
+        password = signInFragment.findViewById(R.id.editTextTextPassword);
+        btnSignIn = signInFragment.findViewById(R.id.btnSignIn);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(getActivity(), "Sign Up Successful!", LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getActivity(), "Invalid email or password", LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return signInFragment;
     }
 }
